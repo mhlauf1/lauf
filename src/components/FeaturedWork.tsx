c822c8d8725cc5cd3311bc1aaab7d8977993ab9c
mhlauf1 <mhlauf1@gmail.com>
2024-10-03 18:06:40 -0500

    hero mobile fixes

--- src/components/FeaturedWork.tsx
+++ src/components/FeaturedWork.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const FeaturedWork = () => {
   return (
     <div className="bg-[#F1EDEA] gap-4 py-24 my-16 flex-col md:flex-row flex-wrap items-center justify-center mx-4 md:mx-20 flex lg:mx-36">
-      <div className="flex flex-col md:flex-row gap-4">
-        <div className="bg-neutral-100 rounded-lg shadow-md w-[400px] md:w-[625px] h-[400px]"></div>
-        <div className="bg-neutral-100 rounded-lg shadow-md w-[400px] md:w-[625px] h-[400px]"></div>
+      <div className="flex flex-col w-full justify-center items-center md:flex-row gap-4">
+        <div className="bg-neutral-100 rounded-lg shadow-md w-5/6 md:w-[625px] h-[400px]"></div>
+        <div className="bg-neutral-100 rounded-lg shadow-md w-5/6 md:w-[625px] h-[400px]"></div>
       </div>
-      <div className="flex flex-col md:flex-row gap-4">
-        <div className="bg-neutral-100 rounded-lg shadow-md w-[400px] md:w-[625px] h-[400px]"></div>
-        <div className="bg-neutral-100 rounded-lg shadow-md w-[400px] md:w-[625px] h-[400px]"></div>
+      <div className="flex flex-col w-full justify-center items-center  md:flex-row gap-4">
+        <div className="bg-neutral-100 items-center rounded-lg shadow-md w-5/6 md:w-[625px] h-[400px]"></div>
+        <div className="bg-neutral-100 rounded-lg shadow-md w-5/6 md:w-[625px] h-[400px]"></div>
       </div>
     </div>
   );
